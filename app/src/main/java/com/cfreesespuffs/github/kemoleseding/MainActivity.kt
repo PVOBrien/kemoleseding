21d231724cd27dfd282698c8b1f54a89e235c626
Paul O'Brien <PVOVideo@outlook.com>
2021-09-16 18:50:11 -0700

    build cards for about organizations.

--- app/src/main/java/com/cfreesespuffs/github/kemoleseding/MainActivity.kt
+++ app/src/main/java/com/cfreesespuffs/github/kemoleseding/MainActivity.kt
@@ -1,37 +1,19 @@
 package com.cfreesespuffs.github.kemoleseding
 
-import android.content.Intent
-import android.graphics.fonts.FontStyle
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -39,7 +21,6 @@ import com.cfreesespuffs.github.kemoleseding.composables.About
 import com.cfreesespuffs.github.kemoleseding.composables.Curriculum
 import com.cfreesespuffs.github.kemoleseding.composables.KemoLesedingTheme
 import com.cfreesespuffs.github.kemoleseding.composables.MDrawerContent
-import com.cfreesespuffs.github.kemoleseding.objModules.*
 import com.cfreesespuffs.github.kemoleseding.ui.theme.kmlRed
 import kotlinx.coroutines.launch
 
